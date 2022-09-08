a40cac611e10590943e3276dc5aabe62077e9f79
nabildroid <pni20156789@gmail.com>
2022-09-08 15:00:53 +0100

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,33 @@
 import 'dart:io';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:lazywalker/cubes/setting_cubit.dart';
 import 'package:lazywalker/cubes/sim_cubit.dart';
 import 'package:lazywalker/cubes/walks_cubit.dart';
+import 'package:lazywalker/repositories/djezzy.dart';
+import 'package:lazywalker/repositories/djezzy_auth.dart';
 import 'package:lazywalker/repositories/local_db.dart';
 import 'package:lazywalker/screens/home1.dart';
 import 'package:lazywalker/services/background.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workmanager/workmanager.dart';
 
+import 'models/check_phone.dart';
+import 'models/djezzy_walk_offer.dart';
+import 'models/walk.dart';
+
 FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin;
 
-void callbackDispatcheraaa() {
+@pragma('vm:entry-point')
+void backgroundHandlerV2() {
   Workmanager().executeTask((task, inputData) async {
     final flutterLocalNotificationsPlugin = await initNotification();
 
     print("TTTTTTTTTTTTTTTTTTTTTASSSSSSSSSK $task");
     if (task == djezzyTask) {
-      return djezzyBackgroundTask(
+      return await djezzyBackgroundTask(
         localNotification: flutterLocalNotificationsPlugin,
       );
     }
@@ -41,30 +49,27 @@ void main() async {
 initWorkManager() async {
   if (Platform.isAndroid) {
     await Workmanager().initialize(
-      callbackDispatcheraaa,
+      backgroundHandlerV2,
       isInDebugMode: true,
     );
   }
 }
 
 initNotification() async {
-  if (Platform.isAndroid) {
-    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-        FlutterLocalNotificationsPlugin();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
-    const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
 
-    const InitializationSettings initializationSettings =
-        InitializationSettings(
-      android: initializationSettingsAndroid,
-    );
-    await flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-    );
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
 
-    return flutterLocalNotificationsPlugin;
-  }
+  return flutterLocalNotificationsPlugin;
 }
 
 showDjezzyBackgroundNotification(
@@ -133,4 +138,139 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+const djezzyTask = 'AAAA';
+const syncDb = 'syncDb';
+
+Future<bool> djezzyBackgroundTask({
+  bool ignoreDelay = false,
+  FlutterLocalNotificationsPlugin? localNotification,
+}) async {
+  final now = DateTime.now();
+
+  final db = LocalDB(await SharedPreferences.getInstance());
+
+  final settings = await db.loadAppSettings();
+  final authorized = await db.getPhoneNumbers();
+
+  if (authorized.isEmpty) return Future.value(true);
+
+  // create not_finished checkPhone Instance
+  late final CheckPhone checkPhone;
+  final lastChecked = await db.getLastCheckedPhoneNumber();
+  if (lastChecked != null && !lastChecked.isFinished) {
+    checkPhone = lastChecked;
+  } else {
+    if (authorized.length > 1) {
+      authorized.removeWhere(
+        (element) => element.phoneNumber == lastChecked?.phone,
+      );
+    }
+    authorized.shuffle();
+    checkPhone = CheckPhone.fromPhone(authorized.first.phoneNumber);
+  }
+
+  final next = authorized.firstWhere(
+    (element) => checkPhone.phone == element.phoneNumber,
+  );
+  final djezzy = Djezzy(DjezzyAuth(next.token));
+
+  final List<Walk> walkes = [];
+
+  // create offers to be applied (or previously not failed _ retry)
+  final List<DjezzyWalkOffer> offers = [];
+  for (var type in settings.offers) {
+    final offer = type.create(next.phoneNumber);
+    if (checkPhone.failedWalks.isEmpty ||
+        checkPhone.failedWalks.containsKey(offer.code)) {
+      offers.add(offer);
+    }
+  }
+
+  for (var i = 0; i < settings.repetition; i++) {
+    // CHECK probably the OS will kill this process before saving the previous walkes
+
+    if (offers.isEmpty) break;
+
+    if (!ignoreDelay) {
+      await Future.delayed(Duration(seconds: i * 10));
+    }
+
+    for (var offer in offers) {
+      bool isSuccess = true;
+
+      final start = DateTime.now();
+
+      try {
+        await djezzy.walkAndWin(offer);
+      } catch (e) {
+        // todo save the error to Log;
+        isSuccess = true;
+      }
+
+      walkes.add(Walk(
+        offer: offer,
+        time: now,
+        isSuccessful: isSuccess,
+        duration: start.difference(DateTime.now()),
+        isBackground: true,
+      ));
+
+      if (isSuccess) {
+        if (localNotification != null) {
+          showDjezzyBackgroundNotification(
+            localNotification,
+            id: Random().nextInt(1000),
+            phone: offer.phoneNumber,
+            code: offer.code,
+            isSuccess: true,
+          );
+        }
+      }
+    }
+
+    // remove successful walks from offers
+    for (var walk in walkes) {
+      if (walk.isSuccessful) {
+        offers.removeWhere((element) => element.code == walk.offer.code);
+      }
+    }
+  }
+
+  // make success on the top &  remove duplication from walks by code
+  walkes.sort((a, b) => (b.isSuccessful ? 1 : 0) - (a.isSuccessful ? 1 : 0));
+  final uniqueWalks = walkes
+      .fold<Map<String, Walk>>({}, (p, c) {
+        p.putIfAbsent(c.offer.code, () => c);
+        return p;
+      })
+      .values
+      .toList();
+
+  for (var walk in uniqueWalks) {
+    await db.saveWalk(walk);
+
+    if (localNotification != null && !walk.isSuccessful) {
+      showDjezzyBackgroundNotification(
+        localNotification,
+        id: Random().nextInt(1000),
+        phone: walk.offer.phoneNumber,
+        code: walk.offer.code,
+        isSuccess: walk.isSuccessful,
+      );
+    }
+  }
+
+  final failedWalkes = uniqueWalks.where((e) => !e.isSuccessful).toList();
+  db.setLastCheckedPhoneNumber(
+    checkPhone.setFailedWalkes(failedWalkes),
+  );
+
+  final isTaskDoneForGood = walkes.every(
+        (element) => element.isSuccessful,
+      ) ||
+      checkPhone.isAboutToFinish;
+
+  return Future.value(isTaskDoneForGood);
 }
